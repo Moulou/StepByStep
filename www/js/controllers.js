608235094ad194977492c2a3fb83bf00bf79b0a1
Hamzouz <hamz.m@hotmail.fr>
2016-05-22 23:02:49 +0200

    reglage BDD + page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,40 +11,75 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('DbCtrl', function($scope) {
-        $scope.myData = new Firebase("https://incandescent-fire-3734.firebaseio.com/Tutos");
+.controller('DbCtrlAdd', function($scope) {
+        $scope.myDataC = new Firebase("https://incandescent-fire-3734.firebaseio.com/Tutos/Cuisine");
+        $scope.myDataS = new Firebase("https://incandescent-fire-3734.firebaseio.com/Tutos/Sport");
+        $scope.myDataB = new Firebase("https://incandescent-fire-3734.firebaseio.com/Tutos/Bricolage");
         $scope.myDataId = new Firebase("https://incandescent-fire-3734.firebaseio.com/Id");
 
         $scope.tutos = {};
 
-        $scope.idCat = 1;
-
-        $scope.higherID = 2;
-
-        $scope.myDataId.update({
-            id:$scope.higherID
-        });
-
-        $scope.myDataId.on('value', function(snapshot) {
-            $scope.tutos = snapshot.val();
-            $scope.$apply();
-        });
-        console.log($scope.tutos.id);
-
         $scope.savaTuto = function() {
-            $scope.myData.push({
-                Id:$scope.idCat,
-                Name:$scope.tutoName,
-                Description:$scope.tutoDescription,
-                Categorie:$scope.tutoCategorie
+            $scope.myDataId.on('value', function(snapshot) {
+                $scope.higherID = snapshot.val().id + 1;
+                $scope.$apply();
             });
+            if($scope.tutoCategorie == "Sport") {
+                $scope.myDataS.push({
+                    Id:$scope.higherID,
+                    Name:$scope.tutoName,
+                    Description:$scope.tutoDescription,
+                    Categorie:$scope.tutoCategorie
+                });
+            } else if($scope.tutoCategorie == "Bricolage") {
+                $scope.myDataB.push({
+                    Id:$scope.higherID,
+                    Name:$scope.tutoName,
+                    Description:$scope.tutoDescription,
+                    Categorie:$scope.tutoCategorie
+                });
+            } else {
+                $scope.myDataC.push({
+                    Id:$scope.higherID,
+                    Name:$scope.tutoName,
+                    Description:$scope.tutoDescription,
+                    Categorie:$scope.tutoCategorie
+                });
+            }
             $scope.tutoName = "";
             $scope.tutoDescription = "";
             $scope.myDataId.update({
                 id:$scope.higherID
             });
         };
+    })
+
+    .controller('DbCtrlCuisine', function($scope) {
+
+        $scope.myDataC.on('value', function(snapshot) {
+            $scope.tutos = snapshot.val();
+            $scope.$apply();
+        });
+    })
+
+    .controller('DbCtrlSport', function($scope) {
+
+        $scope.myDataS = new Firebase("https://incandescent-fire-3734.firebaseio.com/Tutos/Sport");
+        $scope.tutos = {};
+
+        $scope.myDataS.on('value', function(snapshot) {
+            $scope.tutos = snapshot.val();
+            console.log($scope.tutos);
+            $scope.$apply();
+        });
+
+    })
+
+    .controller('DbCtrlBricolage', function($scope) {
+
+        $scope.myDataB.on('value', function(snapshot) {
+            $scope.tutos = snapshot.val();
+            $scope.$apply();
+        });
 
     });
-
-
